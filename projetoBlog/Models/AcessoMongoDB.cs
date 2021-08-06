15a136a22180812fdf0051cca0599c1ed2c23bf7
Everton Oliveira <everton_ace@hotmail.com>
2021-08-05 23:08:40 -0300

    First Commit

---
+++ projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+
+        public const string MONGO_DATABASE = "Blog";
+        public const string COLLECTION_USUARIOS = "Usuarios";
+        public const string COLLECTION_PUBLICACOES = "Publicacoes";
+        public const string CONNECTION = "mongodb://localhost:27017";
+
+        private static readonly IMongoClient client;
+        private static readonly IMongoDatabase db;
+
+        static AcessoMongoDB()
+        {
+            client = new MongoClient(CONNECTION);
+            db = client.GetDatabase(MONGO_DATABASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return client; }
+        }
+
+        public IMongoCollection<Usuario> Usuarios
+        {
+            get { return db.GetCollection<Usuario>(COLLECTION_USUARIOS); }
+        }
+
+        public IMongoCollection<Publicacao> Publicacoes
+        {
+            get { return db.GetCollection<Publicacao>(COLLECTION_PUBLICACOES); }
+        }
+    }
+}
---
+++ projetoBlog/Models/Publicacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace projetoBlog.Models
+{
+    public class Publicacao
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Autor { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public List<string> Tags { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public List<Comentario> Comentarios { get; set; }
+    }
+}
